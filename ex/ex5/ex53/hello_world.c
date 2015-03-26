8ce483fedbe9c1cda0ec7cba1f1664a20bb1f64f
daniw <dani.winz@bluewin.ch>
2015-03-26 17:08:51 +0100

    Exercise 5.3

---
+++ ex/ex5/ex53/hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main(const int argc, const char * argv[])
+    {
+        int i;
+        printf("hello C-world\n");
+        for(i = 0; i < argc; i++) {
+            printf("the %d-th argument is %s\n", i, argv[i]);
+        }
+    return(0);
+}
