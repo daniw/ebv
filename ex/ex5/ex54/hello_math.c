11d88b3c032daa76eeb2c2dff9ec3151f08834e2
daniw <dani.winz@bluewin.ch>
2015-03-26 17:08:57 +0100

    Exercise 5.4

---
+++ ex/ex5/ex54/hello_math.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+main(const int argc, const char * argv[])
+{
+    int i;
+
+    printf("hello C-world\n");
+    printf("cosine(3.14) = %0.5f\n", cos(3.14));
+    return(0);
+}
